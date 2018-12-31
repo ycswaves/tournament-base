06d05c6b517d0ecf90337b17f458653940782e17
chenshu <ycswaves@gmail.com>
2018-12-31 13:59:07 +0800

    refactoring tournament service

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
 import { Tournament } from './models/tournament';
 import { Sandbox } from 'sandbox';
-import {
-  TOURNAMENT_START,
-  GET_TEAM_INFO,
-  GET_MATCH_SCORE,
-  GET_WINNER_SCORE
-} from 'events';
 import { TournamentService } from 'services/TournamentService';
 
 const sandbox = new Sandbox();
 const t = new Tournament(0, 2, 64, sandbox);
-sandbox.register(TOURNAMENT_START, TournamentService.getFirstRound(sandbox));
-sandbox.register(GET_TEAM_INFO, TournamentService.getTeamInfo(sandbox));
-sandbox.register(GET_MATCH_SCORE, TournamentService.getMatchScore(sandbox));
-sandbox.register(GET_WINNER_SCORE, TournamentService.getWinnerScore(sandbox));
-
+const s = new TournamentService(sandbox);
 t.start();
--- src/services/TournamentService.ts
+++ src/services/TournamentService.ts
@@ -7,7 +7,11 @@ import {
   FIRST_ROUND_RECEIVED,
   RECEIVED_TEAM_INFO,
   RECEIVED_MATCH_SCORE,
-  RECEIVED_WINNER_SCORE
+  RECEIVED_WINNER_SCORE,
+  TOURNAMENT_START,
+  GET_TEAM_INFO,
+  GET_MATCH_SCORE,
+  GET_WINNER_SCORE
 } from 'events';
 
 interface FirstRoundDto {
@@ -56,81 +60,82 @@ export interface WinnerScoreResultPayload {
 }
 
 export class TournamentService {
-  public static getWinnerScore(sandbox: Sandbox) {
-    return async (query: WinnerScoreQueryPayload) => {
-      const { tournamentId, match, teamTable } = query;
-      const teamScores = match
-        .getTeamIds()
-        .map(teamId => `teamScores=${teamTable[teamId].score}`)
-        .join('&');
-
-      const uri = `/winner?tournamentId=${tournamentId}&${teamScores}&matchScore=${match.getMatchScore()}`;
-
-      const response = await ServiceClient.get(uri);
-      const winnerScore = parseInt(response.score, 10);
-      sandbox.notify<WinnerScoreResultPayload>({
-        evetName: RECEIVED_WINNER_SCORE,
-        payload: {
-          winnerScore,
-          match
-        }
-      });
-    };
+  constructor(private sandbox: Sandbox) {
+    this.sandbox.register(TOURNAMENT_START, this.getFirstRound);
+    this.sandbox.register(GET_TEAM_INFO, this.getTeamInfo);
+    this.sandbox.register(GET_MATCH_SCORE, this.getMatchScore);
+    this.sandbox.register(GET_WINNER_SCORE, this.getWinnerScore);
   }
 
-  public static getMatchScore(sandbox: Sandbox) {
-    return async (matchScoreQueryPayload: MatchScoreQueryPayload) => {
-      const { tournamentId, match } = matchScoreQueryPayload;
-      const uri = `/match?tournamentId=${tournamentId}&round=${
-        match.roundId
-      }&match=${match.id}`;
+  private getWinnerScore = async (query: WinnerScoreQueryPayload) => {
+    const { tournamentId, match, teamTable } = query;
+    const teamScores = match
+      .getTeamIds()
+      .map(teamId => `teamScores=${teamTable[teamId].score}`)
+      .join('&');
 
-      const response = await ServiceClient.get(uri); // TODO: need mapping
-      const matchScore = parseInt(response.score, 10);
-      sandbox.notify<MatchScoreResultPayload>({
-        evetName: RECEIVED_MATCH_SCORE,
-        payload: { match, matchScore }
-      });
-    };
-  }
+    const uri = `/winner?tournamentId=${tournamentId}&${teamScores}&matchScore=${match.getMatchScore()}`;
 
-  public static getTeamInfo(sandbox: Sandbox) {
-    return async (queryPayload: TeamInfoQueryPayload) => {
-      const { tournamentId, teamId } = queryPayload;
-      const uri = `/team?tournamentId=${tournamentId}&teamId=${teamId}`;
+    const response = await ServiceClient.get(uri);
+    const winnerScore = parseInt(response.score, 10);
+    this.sandbox.notify<WinnerScoreResultPayload>({
+      evetName: RECEIVED_WINNER_SCORE,
+      payload: {
+        winnerScore,
+        match
+      }
+    });
+  };
 
-      const response = await ServiceClient.get(uri);
-      const { name, score } = response;
-      const team = TeamFactory.createTeam(teamId, name, score);
-      sandbox.notify<Team>({
-        evetName: RECEIVED_TEAM_INFO,
-        payload: team
-      });
-    };
-  }
+  private getMatchScore = async (
+    matchScoreQueryPayload: MatchScoreQueryPayload
+  ) => {
+    const { tournamentId, match } = matchScoreQueryPayload;
+    const uri = `/match?tournamentId=${tournamentId}&round=${
+      match.roundId
+    }&match=${match.id}`;
 
-  public static getFirstRound(sandbox: Sandbox) {
-    return async (queryPayload: FirstRoundQueryPayload) => {
-      const { numOfTeams, teamsPerMatch } = queryPayload;
-      const uri = '/tournament';
-      const queryStr = `numberOfTeams=${numOfTeams}&teamsPerMatch=${teamsPerMatch}`;
-      const response: FirstRoundDto = await ServiceClient.post(uri, queryStr);
-      const matchUps = response.matchUps.reduce(
-        (matchUps: FirstRoundMatchUpResponse, next) => {
-          matchUps.teamIds.push(...next.teamIds);
-          const newMatch = new MatchUp(0, next.match, teamsPerMatch).addTeams(
-            next.teamIds
-          );
-          matchUps.matchUps.push(newMatch);
-          return matchUps;
-        },
-        { teamIds: [], matchUps: [] }
-      );
+    const response = await ServiceClient.get(uri); // TODO: need mapping
+    const matchScore = parseInt(response.score, 10);
+    this.sandbox.notify<MatchScoreResultPayload>({
+      evetName: RECEIVED_MATCH_SCORE,
+      payload: { match, matchScore }
+    });
+  };
 
-      sandbox.notify<FirstRoundMatchUpResponse>({
-        evetName: FIRST_ROUND_RECEIVED,
-        payload: matchUps
-      });
-    };
-  }
+  private getTeamInfo = async (queryPayload: TeamInfoQueryPayload) => {
+    const { tournamentId, teamId } = queryPayload;
+    const uri = `/team?tournamentId=${tournamentId}&teamId=${teamId}`;
+
+    const response = await ServiceClient.get(uri);
+    const { name, score } = response;
+    const team = TeamFactory.createTeam(teamId, name, score);
+    this.sandbox.notify<Team>({
+      evetName: RECEIVED_TEAM_INFO,
+      payload: team
+    });
+  };
+
+  private getFirstRound = async (queryPayload: FirstRoundQueryPayload) => {
+    const { numOfTeams, teamsPerMatch } = queryPayload;
+    const uri = '/tournament';
+    const queryStr = `numberOfTeams=${numOfTeams}&teamsPerMatch=${teamsPerMatch}`;
+    const response: FirstRoundDto = await ServiceClient.post(uri, queryStr);
+    const matchUps = response.matchUps.reduce(
+      (matchUps: FirstRoundMatchUpResponse, next) => {
+        matchUps.teamIds.push(...next.teamIds);
+        const newMatch = new MatchUp(0, next.match, teamsPerMatch).addTeams(
+          next.teamIds
+        );
+        matchUps.matchUps.push(newMatch);
+        return matchUps;
+      },
+      { teamIds: [], matchUps: [] }
+    );
+
+    this.sandbox.notify<FirstRoundMatchUpResponse>({
+      evetName: FIRST_ROUND_RECEIVED,
+      payload: matchUps
+    });
+  };
 }
